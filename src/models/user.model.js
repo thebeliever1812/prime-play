6ef65b6765078a2f47603796fb5095ce643c0e40
Basir Ahmad <basirahmadmalik@gmail.com>
2025-09-21 20:01:31 +0530

    Implement user and video models with authentication features and default images

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,21 +1,71 @@
 import { Schema, model } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
-const userSchema = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-        lowerCase: true,
-        trim: true,
-        index: true,
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowerCase: true,
+            trim: true,
+            index: true,
+        },
+        fullName: {
+            type: String,
+            required: true,
+            unique: false,
+            trim: true,
+            index: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowerCase: true,
+            trim: true,
+        },
+        password: {
+            type: String,
+            required: [true, "Password is required"],
+        },
+        avatar: {
+            type: String,
+            default: "/default_avatar.jpg",
+        },
+        coverImage: {
+            type: String,
+            default: "/default_cover_image.jpeg",
+        },
+        watchHistory: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Video",
+            },
+        ],
+        refreshToken: {
+            type: String,
+        },
     },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        lowerCase: true,
-        trim: true,
-    },
+    { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        return next();
+    }
+    try {
+        this.password = await bcrypt.hash(this.password, 10);
+        next();
+    } catch (error) {
+        console.log("Password failed to hashed:", error);
+        next(error);
+    }
 });
 
-export const User = model("User", userSchema)
+userSchema.methods.matchPassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
+
+export const User = model("User", userSchema);
---
+++ src/models/video.model.js
@@ -0,0 +1,44 @@
+import { Schema, model } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        thumbnail: {
+            type: String,
+            required: true,
+        },
+        videoFile: {
+            type: String,
+            required: true,
+        },
+        duration: {
+            type: Number, // Cloudinary se info le lege
+            required: true,
+        },
+        duration: {
+            type: Number,
+            default: 0,
+        },
+        isPublished: {
+            type: Boolean,
+            default: true,
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+        },
+    },
+    { timestamps: true }
+);
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+export const Video = model("Video", videoSchema);
