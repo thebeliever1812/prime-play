7c2163d3a2a89357d89d7c5819451de83f12542b
Basir Ahmad <basirahmadmalik@gmail.com>
2025-10-11 18:56:48 +0530

    changed cookie options

--- src/app.js
+++ src/app.js
@@ -7,16 +7,16 @@ const app = express();
 
 global.accessTokenCookieOptions = {
     httpOnly: true,
-    secure: false,
-    sameSite: "none",
+    secure: true,
+    sameSite: "lax",
     path: "/",
     maxAge: 24 * 60 * 60,
 };
 
 global.refreshTokenCookieOptions = {
     httpOnly: true,
-    secure: false,
-    sameSite: "none",
+    secure: true,
+    sameSite: "lax",
     path: "/",
     maxAge: 10 * 24 * 60 * 60,
 };
