3a71d364d434f379a94f467c872ad3b571cad903
Basir Ahmad <basirahmadmalik@gmail.com>
2025-10-11 21:50:04 +0530

    Update global cookie options; set secure to true and change sameSite attribute to "none"

--- src/app.js
+++ src/app.js
@@ -7,16 +7,16 @@ const app = express();
 
 global.accessTokenCookieOptions = {
     httpOnly: true,
-    secure: false,
-    sameSite: "lax",
+    secure: true,
+    sameSite: "none",
     path: "/",
     maxAge: 24 * 60 * 60,
 };
 
 global.refreshTokenCookieOptions = {
     httpOnly: true,
-    secure: false,
-    sameSite: "lax",
+    secure: true,
+    sameSite: "none",
     path: "/",
     maxAge: 10 * 24 * 60 * 60,
 };
