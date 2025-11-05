048f4ae9526182bfc58b657454f29b4c623369f8
Basir Ahmad <basirahmadmalik@gmail.com>
2025-11-05 20:25:17 +0530

    Fix import statement for ApiResponse in subscribe.controller.js

--- src/controllers/subscribe.controller.js
+++ src/controllers/subscribe.controller.js
@@ -1,6 +1,6 @@
 import { ApiError } from "../utils/ApiError.js";
 import { Subscription } from "../models/subscription.model.js";
-import { ApiResponse } from "../utils/ApiResponse";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 export const handleSubscribeChannel = async (req, res) => {
     if (!req.user) {
